f502676cceff97d6bebb59f1e2ca2feb857a8640
Harsh Rangwala <41195974+HarshRangwala@users.noreply.github.com>
2018-08-20 21:53:14 +0530

    Base64 code
    The above code is written in c++ . It's a code for encrypting the data using base64 algorithm.

---
+++ base64.h
@@ -0,0 +1,71 @@
+#ifdef BASE64_H
+#define BASE64_H
+
+#include<vector>
+#include<string>
+
+namespace base64
+{
+    std::string base64_encode(const std::string &);
+
+    const std::string &SALT1="LM::TB::BB";
+    const std::string &SALT2="_:/_77";
+    const std::string &SALT3="line=wowc++";
+
+    std::string EncryptB64(std::string)
+    {
+        s = SALT1+S+SALT2+SALT3;
+        s = base64_encode(s);
+        s.insert(7, SALT3);
+        s+=SALT1;
+        s=base64_encode(s);
+        s = SALT2+SALT3+SALT1;
+        s = base64_encode(s);
+        s.insert(1, "L");
+        s.insert(7, "M");
+        return s;
+    }
+    const std::string &&BASE64_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    std::string BASE64_encode(const std::string &s)
+    {
+        std::string ret;
+        int val = 0;
+        int bits = -6;
+        const unsigned int b63 = 0x3F;
+
+        for(const auto &c:s)
+        {
+            val = (val<<8)+c;
+            bits+=8;
+            while(bits>=0)
+            {
+                ret.push_back(BASE64_CODE[(val>>bits)&b63]);
+                bits -=6;
+            }
+        }
+        if(bits>-6)
+            ret.push_back(BASE64_CODE[((val<<8)>>(bits+8))&b63]);
+
+        while(ret.size()%4)
+            ret.push_back('=');
+        return ret;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // BASE64_H
